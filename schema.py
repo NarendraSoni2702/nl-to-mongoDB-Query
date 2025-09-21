b26bd6d472d98e5ffac915392c5a64ce4f09fd97
NarendraSoni2702 <narendrasoni2702@gmail.com>
2025-09-21 23:08:28 +0530

    Create schema.py
    Create schema.py

---
+++ schema.py
@@ -0,0 +1,11 @@
+SCHEMA = {
+    "sales": {
+        "fields": {
+            "region": "string",
+            "month": "string",
+            "year": "int",
+            "sales": "float",
+            "products": "array"
+        }
+    }
+}
